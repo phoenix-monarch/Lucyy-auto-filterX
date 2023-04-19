85898d6fbc027c0918ff87f846a900399dd91abc
ʀʏᴜᴋ <111524889+phoenix-monarch@users.noreply.github.com>
2023-04-19 22:55:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PSYCHO-BOTZ/Lucyy-auto-filter.git /DQ-The-File-Donor
+  git clone https://github.com/phoenix-monarch/Lucyy-auto-filter.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
