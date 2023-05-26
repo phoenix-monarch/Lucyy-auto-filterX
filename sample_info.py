8f8c2f054e65c8e4e79ffa7d1eef7f9337a2d4d8
Soul-king7 <101163212+Soul-king7@users.noreply.github.com>
2023-05-26 20:55:27 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 
+API_HASH = ''
+BOT_TOKEN = ''
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
